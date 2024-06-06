c209282de07ed59cc852bebda97fd684f9597bde
arsenid2003@gmail.com <arsenid2003@gmail.com>
2024-06-06 17:08:58 +0500

    qq

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 
 TOKEN = '7409866729:AAFOHZ51bByoojzbKA_5IDGT8MFb9oO3BYE'
-bot = telebot.TeleBot('7409866729:AAFOHZ51bByoojzbKA_5IDGT8MFb9oO3BYE')
+bot = telebot.TeleBot(TOKEN)
 bot.set_webhook()
 
 
