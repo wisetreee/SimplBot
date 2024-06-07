45a5083cab342aa1cf0382478921486273629353
arsenid2003@gmail.com <arsenid2003@gmail.com>
2024-06-07 21:18:40 +0500

    Удалил лишние файлы, добавил кнопку для бота с заглушкой вместо веб-приложения

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,22 @@
 import telebot
-
+from telebot import types
 TOKEN = '7409866729:AAFOHZ51bByoojzbKA_5IDGT8MFb9oO3BYE'
 bot = telebot.TeleBot(TOKEN)
 bot.set_webhook()
 
 
+msg_markup=types.InlineKeyboardMarkup() 
+shop_button = types.InlineKeyboardButton('Магазин',url="https://google.com/")
+# webUrl = types.WebAppInfo("https://google.com/")
+# shop_button = types.InlineKeyboardButton('Магазин',web_app=webUrl)
+# shop_button = types.InlineKeyboardButton('Магазин',web_app=webUrl)
+msg_markup.add(shop_button)
+
+
 @bot.message_handler(commands=['start'])
-def main(message):
-    bot.send_message(message.chat.id, 'Здарова заебал')
-    bot.send_message(message.chat.id, 'как дела? скобочка)')
+def start(message):
+    bot.send_message(message.chat.id, 'Заходи заебал', reply_markup=msg_markup)
    
+
+
 bot.polling(non_stop=True)
-a=5
