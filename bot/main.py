7a92dfe7b657062b2944ef2ce52a5f7ef87f9567
DaVOVAN <vovanova_po4ta@mail.ru>
2024-06-10 18:48:18 +0500

    Добавил в бота команду, кнопку и приветственное сообщение

--- bot/main.py
+++ bot/main.py
@@ -4,18 +4,34 @@ TOKEN = '7409866729:AAFOHZ51bByoojzbKA_5IDGT8MFb9oO3BYE'
 bot = telebot.TeleBot(TOKEN)
 bot.set_webhook()
 
+webAppLink = types.WebAppInfo("https://github.com/wisetreee/SimplBot/blob/main/react-app/public/index.html") #ссылка на наше веб-приложение
 
-msg_markup=types.InlineKeyboardMarkup() 
-# shop_button = types.InlineKeyboardButton('Магазин',url="https://google.com/")
-webUrl = types.WebAppInfo("https://github.com/wisetreee/SimplBot/blob/main/react-app/public/index.html")
-shop_button = types.InlineKeyboardButton('Магазин',web_app=webUrl)
-msg_markup.add(shop_button)
+def webAppMessageButton(): 
+   msg_markup=types.InlineKeyboardMarkup() 
+   button = types.InlineKeyboardButton(text="💰💰💰", web_app=webAppLink) 
+   msg_markup.add(button)
+   return msg_markup
+
+def webAppKeyboard():
+   keyboard = types.ReplyKeyboardMarkup(row_width = 1, resize_keyboard=True)
+   button = types.KeyboardButton(text="Магазин", web_app=webAppLink)
+   keyboard.add(button)
+   return keyboard
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Заходи заебал', reply_markup=msg_markup)
-   
+   bot.send_message( message.chat.id, 'Привет, я SimplBot, твой помощник для работы c валютой SimplCoin!\nЗапустить приложение можно по кнопке ниже или с помощью команды /app.', parse_mode="Markdown", reply_markup=webAppKeyboard())
+   bot.delete_message(message.chat.id, message.message_id)
 
+@bot.message_handler(commands=['app'])
+def app(message):
+   bot.send_message(message.chat.id, 'Ссылка на магазин', reply_markup=webAppMessageButton())
+   bot.delete_message(message.chat.id, message.message_id)
+
+@bot.message_handler(content_types='text')
+def deny(message):
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.send_message(message.chat.id, "Такой команды не существует.")
 
 bot.polling(non_stop=True)
