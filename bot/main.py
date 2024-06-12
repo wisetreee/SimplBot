80ea80f8f91ba97d1f488be0b4b0cc2b327903c9
RoksarOrk <volckov.danil@yandex.ru>
2024-06-12 18:54:04 +0500

    разделил

--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,7 @@
 import telebot
 from telebot import types
-import psycopg2
+from backend import data
 
-
-try:
-    connection = psycopg2.connect(
-        host='localhost',
-        user='postgres',
-        password='123',
-        database='SimplDB'
-      )
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT version();")
-        print(f"Server version {cursor.fetchone()}")
-except Exception as _ex:
-    print('Error with work PostgreSQL',_ex)
 
 
 # ----------------------
@@ -22,7 +9,7 @@ TOKEN = '7409866729:AAFOHZ51bByoojzbKA_5IDGT8MFb9oO3BYE'
 bot = telebot.TeleBot(TOKEN)
 bot.set_webhook()
 
-webAppLink = types.WebAppInfo("https://github.com/wisetreee/SimplBot/blob/main/react-app/public/index.html") #ссылка на наше веб-приложение
+webAppLink = types.WebAppInfo("https://ru.wikipedia.org/wiki/Путин,_Владимир_Владимирович") #ссылка на наше веб-приложение
 
 def webAppMessageButton(): 
    msg_markup=types.InlineKeyboardMarkup() 
@@ -44,7 +31,12 @@ def start(message):
 
 @bot.message_handler(commands=['app'])
 def app(message):
-   bot.send_message(message.chat.id, 'Ссылка на магазин', reply_markup=webAppMessageButton())
+   bot.send_message(message.chat.id, 'Ссылка на магазин ', reply_markup=webAppMessageButton())
+   bot.delete_message(message.chat.id, message.message_id)
+
+@bot.message_handler(commands=['sql'])
+def sql(message):
+   bot.send_message(message.chat.id, data)
    bot.delete_message(message.chat.id, message.message_id)
 
 @bot.message_handler(content_types='text')
