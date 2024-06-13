e8feb63fa0d4af77bc9c080cb56da441a2d46e23
Arseniy Paramonov <paramosha228@gmail.com>
2024-06-13 21:26:38 +0500

    Исправил еще классы

--- bot/DBService.py
+++ bot/DBService.py
@@ -15,34 +15,30 @@ except Exception as _ex:
     print('Error with work PostgreSQL',_ex)  
 
 class Status:
-    id = 0
-    def __init__(self, name):
+    def __init__(self, id, name):
+        self.id = id
         self.name = name
-        Status.id += 1
 
 class Role:
-    id = 0
-    def __init__(self, name):
+    def __init__(self, id, name):
+        self.id = id
         self.name = name
-        Role.id += 1
 
 class Product:
-    id = 0
-    def __init__(self, name, description, price):
+    def __init__(self, id, name, description, price):
+        self.id = id 
         self.name = name
         self.description = description
         self.price = price
-        Product.id += 1
         #ссылка на товар
 
 class User:
-    id = 0
-    def __init__(self, fist_name, last_name, phone_number, balance):
+    def __init__(self, id, fist_name, last_name, phone_number, balance):
+        self.id = id
         self.fist_name = fist_name
         self.fist_name = last_name
         self.phone_number = phone_number
         self.balance = balance
-        User.id += 1
     role_id = Role.id
 
 #TODO переписать метод
@@ -58,32 +54,26 @@ class User:
             return False
 
 class Achievement:
-    id = 0
-    def __init__(self, name, description, reward):
+    def __init__(self, id, name, description, reward):
         self.id = id
         self.name = name
         self.description = description
         self.reward = reward
-        Achievement.id += 1
 
 class Reques_for_coins:
-    id = 0
-    def __init__(self, employee_comment, hr_comment):
+    def __init__(self, id, employee_comment, hr_comment):
         self.id = id
         self.employee_comment = employee_comment
         self.hr_comment = hr_comment
-        Reques_for_coins.id += 1
     id_user = User.id
     id_product = Product.id
     id_status = Status.id
 
 class Request_for_merch:
-    id = 0
-    def __init__(self, employee_comment, hr_comment):
+    def __init__(self, id, employee_comment, hr_comment):
         self.id = id
         self.employee_comment = employee_comment
         self.hr_comment = hr_comment
-        Request_for_merch.id += 1
     id_user = User.id
     id_achievement = Achievement.id
     id_status = Status.id
