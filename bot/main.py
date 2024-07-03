c62de6c34dfd0126a4c5e50396452f4455c7143c
RoksarOrk <volckov.danil@yandex.ru>
2024-07-03 14:33:31 +0500

    тест

--- bot/main.py
+++ bot/main.py
@@ -79,9 +79,9 @@ def getMessage():
     return "!", 200
 
 @app.route('/api/getBalance', methods=['GET']) # При запросе на "https://simplbot.onrender.com/" возвращается JSON-файл
-def get_balance():
-   id = int(request.args.get('user_id'))
-   balance = DBService.get_balance(id)[0] # Метод возвращает список словарей. Что делать, если строк в таблице несколько для одного айдишника?
+def get_balanc():
+   id = int(request.args.get('id_user'))
+   balance = DBService.get_balance(id) # Метод возвращает список словарей. Что делать, если строк в таблице несколько для одного айдишника?
    return jsonify(balance)
 
 # def load_achievements():
