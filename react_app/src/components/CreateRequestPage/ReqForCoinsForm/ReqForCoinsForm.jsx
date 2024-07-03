6f1c7893b34bdd7cde53da86016c20b89adfae1d
arsenid2003@gmail.com <arsenid2003@gmail.com>
2024-07-03 14:24:12 +0500

    тест 2

--- react_app/src/components/CreateRequestPage/ReqForCoinsForm/ReqForCoinsForm.jsx
+++ react_app/src/components/CreateRequestPage/ReqForCoinsForm/ReqForCoinsForm.jsx
@@ -12,7 +12,7 @@ const ReqForCoinsForm = () => {
   
 
   useEffect(() => {
-    tg.MainButton.setBackgroundColor('#F23D3D');
+    
     // Fetch achievements from the backend
     const fetchAchievements = async () => {
       try {
@@ -32,6 +32,7 @@ const ReqForCoinsForm = () => {
     // Update the main button state based on form completeness
    
     tg.MainButton.setParams({ text: "Отправить" });
+    tg.MainButton.color = '#f23d3d';
 
     if (selectedAchievement && comment) {
       tg.MainButton.show();
